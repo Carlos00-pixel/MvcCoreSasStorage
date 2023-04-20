9facf3f266f3c8a3f71227a794098aea452c3d60
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-04-20 14:22:48 +0200

    Add project files.

---
+++ MvcCoreSasStorage/Controllers/AlumnosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSasStorage.Models;
+using MvcCoreSasStorage.Services;
+
+namespace MvcCoreSasStorage.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        private ServiceStorageAlumnosXML service;
+
+        public AlumnosController(ServiceStorageAlumnosXML service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            List<Alumno> alumnos = this.service.MostrarAlumnos();
+            return View(alumnos);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            List<Alumno> listalumno = this.service.MostrarAlumnos();
+            await this.service.InsertarAlumnos(listalumno);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreSasStorage/Services/ServiceStorageAlumnosXML.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Data.Tables;
+using MvcCoreSasStorage.Helpers;
+using MvcCoreSasStorage.Models;
+using System.Xml.Linq;
+
+namespace MvcCoreSasStorage.Services
+{
+    public class ServiceStorageAlumnosXML
+    {
+        private TableClient tableAlumno;
+        private HelperPathProvider helper;
+
+        public ServiceStorageAlumnosXML
+            (TableServiceClient tableService, HelperPathProvider helper)
+        {
+            this.tableAlumno = tableService.GetTableClient("alumnos");
+            Task.Run(async () =>
+            {
+                await this.tableAlumno.CreateIfNotExistsAsync();
+            });
+            this.helper = helper;
+        }
+
+        public List<Alumno> MostrarAlumnos()
+        {
+            string path = helper.MapPath("alumnos_tables.xml", Folders.Documents);
+
+            XDocument document = XDocument.Load(path);
+            List<Alumno> alumnos = new List<Alumno>();
+            var consulta = from datos in document.Descendants("alumno")
+                           select datos;
+
+            foreach (XElement tag in consulta)
+            {
+                Alumno alumno = new Alumno();
+
+                alumno.IdAlumno = int.Parse(tag.Element("idalumno").Value);
+                alumno.Curso = tag.Element("curso").Value;
+                alumno.Nombre = tag.Element("nombre").Value;
+                alumno.Apellidos = tag.Element("apellidos").Value;
+                alumno.Nota = int.Parse(tag.Element("nota").Value);
+
+                alumnos.Add(alumno);
+            }
+
+            return alumnos;
+        }
+
+        public async Task InsertarAlumnos(List<Alumno> alumnos)
+        {
+            foreach(Alumno item in alumnos)
+            {
+               await this.tableAlumno.AddEntityAsync<Alumno>(item);
+            }
+        }
+    }
+}
